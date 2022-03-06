e792c3475cb0dd1d300e07baabc6e104d081c9e5
FunnyMan11 <kobuladze.nk1@gmail.com>
2022-03-07 00:42:37 +0400

    added color variables

--- styled/theme/theme.ts
+++ styled/theme/theme.ts
@@ -1,6 +1,11 @@
 const theme = {
   color: {
     black: "#000",
+    whitesmoke: "#F4F4F4",
+    liteblue:"#567895",
+    litegray: "#BFC9D1",
+    carrot: "#CE8B65"
+    
   },
   breackpoints: {
     xs: `(max-width: ${320}px)`,
